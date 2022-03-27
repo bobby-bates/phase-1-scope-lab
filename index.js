ab76418efeea8b7940c2415be2f90081b0d825d5
Bobby Bates <robertfbatesjr@gmail.com>
2022-03-27 17:27:24 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+var upperCaseCustomerName = () => customerName = customerName.toUpperCase()
+
+var setBestCustomer = () => bestCustomer = 'not bob'
+
+var overwriteBestCustomer = newCustomer => bestCustomer = newCustomer
+
+const leastFavoriteCustomer = 'leeroy'
+
+var changeLeastFavoriteCustomer = () => leastFavoriteCustomer = 'steve'
